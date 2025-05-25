d31d61312f4851e1d4701fba788d67f3f7f51f1d
geekycoderpamir <h.jumaev@gmail.com>
2025-05-24 23:18:02 -0400

    Add course model and initial migration

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    icon_class = models.CharField(max_length=50)
+    detail_link = models.URLField(blank=True)
+
+    def _str__(self):
+        return self.title
