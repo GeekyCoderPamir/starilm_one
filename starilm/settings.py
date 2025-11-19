b1620428dd139981ce01ab7689956cfb99b84591
Hamroz Jumaev <46858931+GeekyCoderPamir@users.noreply.github.com>
2025-11-18 23:44:22 -0500

    Update settings.py

--- starilm/settings.py
+++ starilm/settings.py
@@ -16,12 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0#ze3=f3u6uips!k6xm2k5_!c_usatkx(s$*wi%qg%v4^ihn0!"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
